1defa95a083b134b094ea560b93b04660f9b399a
zhaochenhang <zhaochenhang@qq.com>
2024-02-20 13:47:30 +0800

    部门管理功能完成

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from app01.models import Depart
+
 
 # Create your views here.
+
+def depart_list(request):
+    depart_data = Depart.objects.all()
+
+    return render(request, 'depart_list.html', {'depart_data': depart_data})
+
+
+def depart_add(request):
+    if request.method == "GET":
+        return render(request, 'depart_add.html')
+
+    depart_title = request.POST.get('title')
+    exists = Depart.objects.filter(title=depart_title).exists()
+    if not exists:
+        Depart.objects.create(title=depart_title)
+    else:
+        return render(request, 'depart_add.html', {'error_msg': '部门名称存在请重新输入'})
+
+    return redirect('/depart/list/')
+
+
+def depart_delete(request, nid):
+    Depart.objects.filter(id=nid).delete()
+    return redirect('/depart/list/')
+
+
+def depart_edit(request, nid):
+    if request.method == "GET":
+        depart_info = Depart.objects.filter(id=nid).first()
+        return render(request, 'depart_edit.html', {'depart_info': depart_info})
+
+    title_new = request.POST.get('title')
+    Depart.objects.filter(id=nid).update(title=title_new)
+    return redirect('/depart/list/')
+
+
+
